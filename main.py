3d4fd0fd1866f30154ebedbf0ae51f665aeeed1c
roger <A127038349@gmail.com>
2026-04-13 08:11:18 +0800

    完成後端數據傳遞到前端 透過jinja2迴圈顯示表格

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
+from pm25 import get_open_data
+
+books = {
+    1: {
+        "name": "Python book",
+        "price": 299,
+        "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348",
+    },
+    2: {
+        "name": "Java book",
+        "price": 399,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348",
+    },
+    3: {
+        "name": "C# book",
+        "price": 499,
+        "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348",
+    },
+}
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    time = now_time()
+    return render_template("index.html", time=time, name="Roger")
 
 
 @app.route("/nowtime")
@@ -20,11 +40,9 @@ def now_time():
 @app.route("/books/id=<int:id>")
 def get_books(id=None):
     try:
-        books = {1: "Python book", 2: "Java book", 3: "C++ book"}
         if id is None:
-            return books
-        else:
-            return books[id]
+            return render_template("books.html", books=books)
+        return render_template("book.html", book=books[id])
     except Exception as e:
         return f"書籍編號錯誤:{e}"
 
@@ -36,6 +54,14 @@ def get_bmi(h, w):
         return f"<h1>身高:{h}m,體重:{w}kg,BMI值為:{bmi}</h1>"
     except Exception as e:
         return f"身高或體重輸入錯誤:{e}"
+
+
+@app.route("/pm25")
+def get_pm25():
+    values = get_open_data()
+    print(values)
+    columns = ["站點名稱", "縣市", "PM2.5", "更新時間", "單位"]
+    return render_template("pm25.html", values=values, columns=columns)
 
 
 app.run(debug=True)
